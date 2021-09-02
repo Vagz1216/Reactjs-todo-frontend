94de1f55f5eed5b1c0202a0b117475c7651e0028
vagz <you@example.com>
2021-09-02 07:51:17 +0300

    third commit-modals

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Modal from "./components/Modal";
 
 const todoItems = [
   {
@@ -33,9 +34,36 @@ class App extends Component {
     this.state = {
       viewCompleted: false,
       todoList: todoItems,
+      modal: false,
+      activeItem:{
+        title: "",
+        description: "",
+        completed: false,
+      },
     };
   }
+  toggle = () => {
+    this.setState({ modal: !this.state.modal });
+  };
+  handleSubmit = (item) => {
+    this.toggle();
 
+    alert("save" + JSON.stringify(item));
+  };
+  
+  handleDelete = (item) => {
+    alert("delete" + JSON.stringify(item));
+  };
+
+  createItem = () => {
+    const item = { title: "", description: "", completed: false };
+
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+
+  editItem = (item) => {
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
   displayCompleted = (status) => {
     if (status) {
       return this.setState({ viewCompleted: true });
@@ -66,7 +94,7 @@ class App extends Component {
   renderItems = () => {
     const { viewCompleted } = this.state;
     const newItems = this.state.todoList.filter(
-      (item) => item.completed == viewCompleted
+      (item) => item.completed === viewCompleted
     );
 
     return newItems.map((item) => (
@@ -75,7 +103,8 @@ class App extends Component {
         className="list-group-item d-flex justify-content-between align-items-center"
       >
         <span
-          className={`todo-title mr-2 ${
+          className={`todo-title mr-2 
+          ${
             this.state.viewCompleted ? "completed-todo" : ""
           }`}
           title={item.description}
@@ -85,11 +114,13 @@ class App extends Component {
         <span>
           <button
             className="btn btn-secondary mr-2"
+            onClick={() => this.editItem(item)}
           >
             Edit
           </button>
           <button
             className="btn btn-danger"
+            onClick={() => this.handleDelete(item)}
           >
             Delete
           </button>
@@ -103,22 +134,30 @@ class App extends Component {
       <main className="container">
         <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
         <div className="row">
-          <div className="col-md-6 col-sm-10 mx-auto p-0">
+           <div className="col-md-6 col-sm-10 mx-auto p-0"> 
             <div className="card p-3">
               <div className="mb-4">
                 <button
                   className="btn btn-primary"
+                  onClick={this.createItem}
                 >
                   Add task
                 </button>
               </div>
               {this.renderTabList()}
               <ul className="list-group list-group-flush border-top-0">
-                {this.renderItems()}
+              {this.renderItems()}
               </ul>
             </div>
           </div>
         </div>
+        {this.state.modal ? (
+          <Modal
+            activeItem={this.state.activeItem}
+            toggle={this.toggle}
+            onSave={this.handleSubmit}
+          />
+        ) : null}
       </main>
     );
   }
